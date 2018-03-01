e2c478af8b66c26d7ec57d29a07dea7c615d58f1
Matthew Schleindl <mattschleindl@yahoo.com>
2018-03-01 07:04:51 -0500

    remove service worker and don't allow higher versions of manifold-dx

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+// import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
 ReactDOM.render(

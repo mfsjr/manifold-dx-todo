58b7d08eb8c22af7efee6b3df3be2735bdb64f84
Matthew Schleindl <mattschleindl@yahoo.com>
2018-06-16 10:26:09 -0400

    remove unused code

--- src/presenters/FilterLinkView.tsx
+++ src/presenters/FilterLinkView.tsx
@@ -24,27 +24,3 @@ export function FilterLinkViewSfc(props: FilterLinkViewProps): ReactElement<Filt
     </a>
   );
 }
-
-// export class FilterLinkView extends React.PureComponent<FilterLinkViewProps, any> {
-//
-//   constructor(props: FilterLinkViewProps) {
-//     super(props);
-//   }
-//
-//   public render(): React.ReactNode {
-//     if (this.props.active) {
-//       return <span>{this.props.children}</span>;
-//     }
-//     return (
-//       <a
-//         href="#"
-//         onClick={e => {
-//           e.preventDefault();
-//           this.props.onClick();
-//         }}
-//       >
-//         {this.props.children}
-//       </a>
-//     );
-//   }
-// }
